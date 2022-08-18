02922b12af7c94a25f418f73067efc6ed9973935
cmachin <carlos.machin@gmail.com>
2022-08-18 13:58:53 -0400

    container sketch

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,25 @@ function App() {
 	return (
 		<div>
 			<div className="banner">
-				<div className="logo">Data Visualization App</div>
+				<div className="logo">Bathymetry Observation Boat</div>
+			</div>
+			<h1>Data Visualization</h1>
+			<div className="app-container">
+				<div className="nav-bar">
+					<div className="heading">
+						<h2>Sensor</h2>
+					</div>
+				</div>
+				<div className="options-card">
+					<div className="heading">
+						<h2>Options</h2>
+					</div>
+				</div>
+				<div className="graph-card">
+					<div className="heading">
+						<h2>Graph</h2>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
