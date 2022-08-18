2f396c24df551f3018ccdf9ae4c3755c24c0c8ad
cmachin <carlos.machin@gmail.com>
2022-08-18 12:29:20 -0400

    code refactoring

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+	return (
+		<div>
+			<div className="banner">
+				<div className="logo">Data Visualization App</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
