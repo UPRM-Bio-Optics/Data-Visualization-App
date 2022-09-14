7889d591252eaadc22ddb6f4da4b548c1c4ab161
cmachin <carlos.machin@gmail.com>
2022-09-14 16:28:15 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "App.tsx";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
